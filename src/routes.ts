509e6e1f61aa23347aa201f3149c7dddee3d4cf9
Fagner <meurerfagner@hotmail.com>
2020-10-16 19:25:49 -0300

    NLW3 aula 2

---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
+import OrphanageController from './controllers/OrphanageController';
+
+
+const router = Router();
+const upload = multer(uploadConfig);
+
+router.get('/orphanages', OrphanageController.index);
+router.get('/orphanages/:id', OrphanageController.show);
+router.post('/orphanages',upload.array('images'), OrphanageController.create);
+
+export default router;
